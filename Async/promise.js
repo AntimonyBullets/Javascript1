6ac9bdbee071580377ed72c578e1ef5e25430909
AntimonyBullets <110652473+AntimonyBullets@users.noreply.github.com>
2024-10-30 17:08:15 +0000

    commit-21

--- Async/promise.js
+++ Async/promise.js
@@ -6,7 +6,27 @@ let xPromise = new Promise(function(resolve,reject){
     }, 1000);
 })
 xPromise.then(()=>{
-    console.log("The task completed successfully");
+    console.log("The task 1 completed successfully");
 }) // the callback inside then() will be passed as the first parameter of the function inside Promise ('resolve' in this case). Similarly the callback inside catch() will be passed as the second paramter of the function inside Promise ('reject' in this case).
 
 // There's another way to perform this without storing the instance into a variable
+
+new Promise((resolve, reject)=>{
+    setTimeout(() => {
+        console.log("Async tast 2 running...");
+        resolve();
+    }, 1000);
+}).then(()=>{
+    console.log("The task 2 completed successfully");
+})
+
+// Following is the way to pass arguments in the callback function inside then() ('resolve()' in this case)
+let yPromise = new Promise((resolve, reject)=>{
+    setTimeout(() => {
+        console.log("Async task 3 is running..."); 
+        resolve({username: "xyz", email: "example@example.com"}) // calling resolve() with an object passed as an argument.
+    }, 1000);
+})
+yPromise.then(function(user){
+    console.log(user);
+})
